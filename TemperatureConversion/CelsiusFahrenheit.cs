dd6e4f5575c8dfab106aa9bcaded79249a89a69a
gerry lowry <gerry.lowry@abilitybusinesscomputerservices.com>
2016-08-23 12:09:14 -0400

    adding .NET 4.5.2 solution files

---
+++ TemperatureConversion/CelsiusFahrenheit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConversion
+{
+    public class CelsiusFahrenheit
+    {
+        public static Double Celsius(Double fahrenheit)
+        {
+            return (fahrenheit - 32d) / 1.8d;
+        }
+
+        public static Double Fahrenheit(Double celsius)
+        {
+            return (celsius * 1.8d) + 32d;
+        }
+
+    }
+}
